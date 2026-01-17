d2c1346835844a09c33957691e1692301fe4cb75
Hassan Bahaa <66232742+hassanbahaa@users.noreply.github.com>
2026-01-17 02:18:23 +0200

    feat:add DELETE /user with id

--- index.js
+++ index.js
@@ -73,6 +73,40 @@ app.patch("/user/:id", express.json(), (req, res, next) => {
   });
 });
 
+// delete user data
+app.delete("/user/:id", (req, res, next) => {
+  const userId = req.params.id;
+  fs.readFile("users.json", "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ message: "Error read file" });
+    }
+    if (data !== "") {
+      users = JSON.parse(data);
+    } else {
+      return res.json({
+        success: false,
+        message: "no users found",
+      });
+    }
+  });
+  //check if the id is correct
+  const userIndex = users.findIndex((user) => user.id == userId);
+
+  if (userIndex === -1) {
+    return res.status(404).json({
+      message: "user Id not found",
+      success: false,
+    });
+  }
+  users.splice(userIndex, 1);
+
+  fs.writeFileSync("users.json", JSON.stringify(users));
+
+  res.json({
+    message: "User deleted successfully",
+  });
+});
+
 app.listen(port, () => {
   console.log("The server is running on port:", port);
 });
